7e3be505d0c013b681dd06cd2c9ed6d56d61d723
alemuntoni <alemuntoni@users.noreply.github.com>
2025-06-05 14:56:05 +0200

    background color

--- application.h
+++ application.h
@@ -95,6 +95,33 @@ public:
         // class attribute)
         m_queue = wgpuDeviceGetQueue(m_device);
 
+        WGPUSurfaceConfiguration config = WGPU_SURFACE_CONFIGURATION_INIT;
+
+        // Configuration of the textures created for the underlying swap chain
+        config.width = 640;
+        config.height = 480;
+        config.device = m_device;
+        // We initialize an empty capability struct:
+        WGPUSurfaceCapabilities capabilities = WGPU_SURFACE_CAPABILITIES_INIT;
+
+        // We get the capabilities for a pair of (surface, adapter).
+        // If it works, this populates the `capabilities` structure
+        WGPUStatus status = wgpuSurfaceGetCapabilities(m_surface, adapter, &capabilities);
+        if (status != WGPUStatus_Success) {
+            return false;
+        }
+
+        // From the capabilities, we get the preferred format: it is always the first one!
+        // (NB: There is always at least 1 format if the GetCapabilities was successful)
+        config.format = capabilities.formats[0];
+
+        // We no longer need to access the capabilities, so we release their memory.
+        wgpuSurfaceCapabilitiesFreeMembers(capabilities);
+        config.presentMode = WGPUPresentMode_Fifo;
+        config.alphaMode = WGPUCompositeAlphaMode_Auto;
+
+        wgpuSurfaceConfigure(m_surface, &config);
+
         // We no longer need to access the adapter
         wgpuAdapterRelease(adapter);
 
@@ -104,6 +131,7 @@ public:
     // Uninitialize everything that was initialized
     void terminate()
     {
+        wgpuSurfaceUnconfigure(m_surface);
         wgpuQueueRelease(m_queue);
         wgpuSurfaceRelease(m_surface);
         wgpuDeviceRelease(m_device);
@@ -116,10 +144,65 @@ public:
     {
         glfwPollEvents();
         wgpuInstanceProcessEvents(m_instance);
+
+        WGPUTextureView targetView = getNextSurfaceView();
+        if (!targetView) return; // no surface texture, we skip this frame
+        WGPUCommandEncoderDescriptor encoderDesc = WGPU_COMMAND_ENCODER_DESCRIPTOR_INIT;
+        encoderDesc.label = toWgpuStringView("My command encoder");
+        WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(m_device, &encoderDesc);
+        WGPURenderPassDescriptor renderPassDesc = WGPU_RENDER_PASS_DESCRIPTOR_INIT;
+        WGPURenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_INIT;
+
+        colorAttachment.view = targetView;
+        colorAttachment.loadOp = WGPULoadOp_Clear;
+        colorAttachment.storeOp = WGPUStoreOp_Store;
+        colorAttachment.clearValue = WGPUColor{ 0.5, 0.5, 0.5, 1.0 };
+
+        renderPassDesc.colorAttachmentCount = 1;
+        renderPassDesc.colorAttachments = &colorAttachment;
+
+        WGPURenderPassEncoder renderPass = wgpuCommandEncoderBeginRenderPass(encoder, &renderPassDesc);
+        // Use the render pass here (we do nothing with the render pass for now)
+        wgpuRenderPassEncoderEnd(renderPass);
+        wgpuRenderPassEncoderRelease(renderPass);
+        WGPUCommandBufferDescriptor cmdBufferDescriptor = WGPU_COMMAND_BUFFER_DESCRIPTOR_INIT;
+        cmdBufferDescriptor.label = toWgpuStringView("Command buffer");
+        WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmdBufferDescriptor);
+        wgpuCommandEncoderRelease(encoder); // release encoder after it's finished
+
+        // Finally submit the command queue
+        wgpuQueueSubmit(m_queue, 1, &command);
+        wgpuCommandBufferRelease(command);
+        // At the end of the frame
+        wgpuTextureViewRelease(targetView);
+#ifndef __EMSCRIPTEN__
+        wgpuSurfacePresent(m_surface);
+#endif
     }
 
     // Return true as long as the main loop should keep on running
     bool isRunning() const { return !glfwWindowShouldClose(m_window); }
+
+private:
+    WGPUTextureView getNextSurfaceView()
+    {
+        WGPUSurfaceTexture surfaceTexture = WGPU_SURFACE_TEXTURE_INIT;
+        wgpuSurfaceGetCurrentTexture(m_surface, &surfaceTexture);
+        if (
+            surfaceTexture.status != WGPUSurfaceGetCurrentTextureStatus_SuccessOptimal &&
+            surfaceTexture.status != WGPUSurfaceGetCurrentTextureStatus_SuccessSuboptimal
+            ) {
+            return nullptr;
+        }
+        WGPUTextureViewDescriptor viewDescriptor = WGPU_TEXTURE_VIEW_DESCRIPTOR_INIT;
+        viewDescriptor.label = toWgpuStringView("Surface texture view");
+        viewDescriptor.dimension = WGPUTextureViewDimension_2D; // not to confuse with 2DArray
+        WGPUTextureView targetView = wgpuTextureCreateView(surfaceTexture.texture, &viewDescriptor);
+        // We no longer need the texture, only its view,
+        // so we release it at the end of GetNextSurfaceViewData
+        wgpuTextureRelease(surfaceTexture.texture);
+        return targetView;
+    }
 };
 
 #endif // APPLICATION_H
